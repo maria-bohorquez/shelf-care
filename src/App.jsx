5a6252a62c37aa537174cc5f85e68fe12a89b0f2
María Bohórquez <a.maria.bohorquez@gmail.com>
2021-10-19 08:12:34 -0500

    API setup

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  console.log("This message will render every time the component renders.");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://book-club-json.herokuapp.com/books"
+        );
+        console.log(`Here's what our fetch request returns`, response);
+
+        const books = await response.json();
+        console.log(`Our json-ified response :`, books);
+        setBooks(books);
+      } catch (errors) {
+        console.log(errors);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(`The books array in our state:`, books);
+  return <div>Hello there</div>;
+};
+export default App;
